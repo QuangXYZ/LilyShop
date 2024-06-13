c4b14ee8eaa3471c93f4a8814d952eed9824bab9
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2024-06-13 20:47:56 +0700

    Detail Activity Backend

--- app/src/main/java/com/quang/lilyshop/Model/ProductModel.kt
+++ app/src/main/java/com/quang/lilyshop/Model/ProductModel.kt
@@ -1,5 +1,7 @@
 package com.quang.lilyshop.Model
 
+import android.os.Parcel
+import android.os.Parcelable
 import android.service.quicksettings.Tile
 
 data class ProductModel(
@@ -10,4 +12,36 @@ data class ProductModel(
     var size: ArrayList<String> = ArrayList(),
     var rating: Double = 0.0,
     var numberInCart: Int = 0
-)
+):Parcelable{
+    constructor(parcel: Parcel):this(
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readDouble(),
+        parcel.createStringArrayList() as ArrayList<String>,
+        parcel.createStringArrayList() as ArrayList<String>,
+        parcel.readDouble()
+    )
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, p1: Int) {
+     dest.writeString(title)
+     dest.writeString(description)
+     dest.writeStringList(picUrl)
+     dest.writeStringList(size)
+     dest.writeDouble(price)
+     dest.writeDouble(rating)
+    }
+
+    companion object CREATOR : Parcelable.Creator<ProductModel> {
+        override fun createFromParcel(parcel: Parcel): ProductModel {
+            return ProductModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ProductModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
