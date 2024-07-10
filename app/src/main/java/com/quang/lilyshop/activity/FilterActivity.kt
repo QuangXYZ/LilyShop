8382afb0b73d57593367ebc10d398554f60fb92c
Nguyen Quang <thanhquang.lord@gmail.com>
2024-07-10 21:28:43 +0700

    feat: add search screen

---
+++ app/src/main/java/com/quang/lilyshop/activity/FilterActivity.kt
@@ -0,0 +1,12 @@
+package com.quang.lilyshop.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.quang.lilyshop.R
+
+class FilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter)
+    }
+}
--- app/src/main/java/com/quang/lilyshop/activity/Fragment/SearchFragment.kt
+++ app/src/main/java/com/quang/lilyshop/activity/Fragment/SearchFragment.kt
@@ -1,26 +1,48 @@
 package com.quang.lilyshop.activity.Fragment
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import com.quang.lilyshop.R
+import com.quang.lilyshop.activity.CartActivity
+import com.quang.lilyshop.activity.CheckoutActivity
+import com.quang.lilyshop.activity.FilterActivity
+import com.quang.lilyshop.databinding.FragmentHomeBinding
+import com.quang.lilyshop.databinding.FragmentSearchBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        settingUpListener()
+    }
+
+    private fun settingUpListener() {
+        binding.searchText.setEndIconOnClickListener(View.OnClickListener {
+            activity?.startActivity(Intent(context, FilterActivity::class.java))
+
+        })
+
+    }
+
 
 }
