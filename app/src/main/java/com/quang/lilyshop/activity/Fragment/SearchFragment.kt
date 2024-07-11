3fcd4f41965a9b09ff0d13de4272f840a88a72ef
Nguyen Quang <thanhquang.lord@gmail.com>
2024-07-11 20:10:31 +0700

    feat: add filter screen

--- app/src/main/java/com/quang/lilyshop/activity/Fragment/SearchFragment.kt
+++ app/src/main/java/com/quang/lilyshop/activity/Fragment/SearchFragment.kt
@@ -1,24 +1,22 @@
 package com.quang.lilyshop.activity.Fragment
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import com.quang.lilyshop.R
-import com.quang.lilyshop.activity.CartActivity
-import com.quang.lilyshop.activity.CheckoutActivity
-import com.quang.lilyshop.activity.FilterActivity
-import com.quang.lilyshop.databinding.FragmentHomeBinding
+import com.quang.lilyshop.databinding.ActivityMainBinding
 import com.quang.lilyshop.databinding.FragmentSearchBinding
 
 
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
+    private lateinit var searchResultFragment: SearchResultFragment
+    private lateinit var filterFragment: FilterFragment
+    private lateinit var recentFragment: SearchRecentFragment
+    private var activeFragment: Fragment? = null
+    private var isFilterOpen = false
 
 
     override fun onCreateView(
@@ -34,12 +32,37 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         settingUpListener()
+
+        searchResultFragment = SearchResultFragment()
+        filterFragment = FilterFragment()
+        recentFragment = SearchRecentFragment()
+        activeFragment = recentFragment
+
+        childFragmentManager.beginTransaction().apply {
+            add(binding.contentFrame.id, recentFragment, "recentFragment").show(recentFragment)
+            add(binding.contentFrame.id, filterFragment, "filterFragment").hide(filterFragment)
+            add(binding.contentFrame.id, searchResultFragment, "searchResultFragment").hide(searchResultFragment)
+        }.commit()
     }
 
     private fun settingUpListener() {
         binding.searchText.setEndIconOnClickListener(View.OnClickListener {
-            activity?.startActivity(Intent(context, FilterActivity::class.java))
+            if (isFilterOpen) {
 
+                childFragmentManager.beginTransaction().apply {
+                    hide(filterFragment!!)
+                    activeFragment?.let { it1 -> show(it1) }
+                }.commit()
+                isFilterOpen = false
+            }
+            else {
+                childFragmentManager.beginTransaction().apply {
+                    hide(activeFragment!!)
+                    show(filterFragment)
+                }.commit()
+                isFilterOpen = true
+
+            }
         })
 
     }
